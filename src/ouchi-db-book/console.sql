f8797714cb87953dc265033c1e442d27baf7a4e7
nukopy <nukopy@gmail.com>
2025-10-21 17:01:05 +0900

    chore: format

--- src/ouchi-db-book/console.sql
+++ src/ouchi-db-book/console.sql
@@ -1,14 +1,20 @@
-SHOW STATUS LIKE 'Uptime';
+SHOW STATUS like 'Uptime';
 
-SHOW STATUS LIKE 'Threads_connected';
+SHOW STATUS like 'Threads_connected';
 
-SELECT *
-FROM performance_schema.global_variables
-WHERE variable_name = 'admin_port';
+SELECT
+	*
+FROM
+	performance_schema.global_variables
+WHERE
+	variable_name = 'admin_port';
 
-SELECT variable_name,
-       variable_value
-FROM performance_schema.global_variables
-WHERE variable_name = 'pid_file';
+SELECT
+	variable_name,
+	variable_value
+FROM
+	performance_schema.global_variables
+WHERE
+	variable_name = 'pid_file';
 
 SHOW DATABASES;
